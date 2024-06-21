3bced9bc859c4dec6e7eb7abcc07b59230c727b9
johnny jorgesvega <johnny.ccgroup@gmail.com>
2024-06-21 10:49:45 -0500

    21_06a

--- src/components/subirImagenMoto/contentSubirImagenMoto.jsx
+++ src/components/subirImagenMoto/contentSubirImagenMoto.jsx
@@ -1,14 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
-import { Button } from '@mui/material';
+import { Button, Modal, TextField } from '@mui/material';
 
-export default function contentSubirImagenMoto() {
+export default function ContentSubirImagenMoto() {
 
     const [usernamePart, setUsernamePart] = useState('');
     const [username1, setUsername1] = useState('');
     const [incidentes, setIncidentes] = useState([]);
+    const [idImg, setIDImg] = useState('');
+    const [image, setImage] = useState(null);
+    const [selectedIncident, setSelectedIncident] = useState(null);
     const [countIncidentes, setCountIncidentes] = useState(0);
+    const [modalUpOpen, setModalUpOpen] = useState(false);
+    const [options, setOptions] = useState([]);
     const todayMarkets = new Date();
+
+    const handleOpenUploadModal = (incident) => {
+        setIDImg(incident.id);
+        setSelectedIncident(incident);
+        setModalUpOpen(true);
+    };
+    const handleCloseUpModal = () => {
+        setModalUpOpen(false);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('photo', image); // Asegúrate de que 'photo' sea el nombre correcto del campo de imagen en el backend
+        formData.append('id_corte', idImg); // Asumiendo que 'client' es el ID del corte que necesita el backend
+
+        try {
+            const response = await fetch('https://api.cvimport.com/api/updaloadImage', {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.text();
+            console.log(data);
+            alert("Tu imagen fue subida. cargando.....")
+        } catch (error) {
+            console.error('Error al cargar la imagen:', error);
+        }
+        handleCloseUpModal();
+        obtenerIncidentes();
+    };
+
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    };
 
     const obtenerIncidentes = async () => {
         try {
@@ -35,6 +75,16 @@ export default function contentSubirImagenMoto() {
 
                 setIncidentes(incidentesFiltrados);
                 setCountIncidentes(incidentesFiltrados.length);
+
+                // Formatear datos para el select
+                const formattedOptions = incidentesFiltrados.map(incidente => ({
+                    value: incidente.id,
+                    label: `${incidente.id} - ${incidente.client} - ${incidente.phone}`,
+                    data: incidente
+                }));
+
+                setOptions(formattedOptions);
+
             } else {
                 console.error("Error de fetch", response.statusText);
             }
@@ -42,7 +92,7 @@ export default function contentSubirImagenMoto() {
             console.error("Error de bd", error);
         }
     };
-    
+
     const formatDateTime = (date, timeZone) => {
         const options = {
             year: 'numeric',
@@ -55,13 +105,20 @@ export default function contentSubirImagenMoto() {
     };
     const formattedDateTime = formatDateTime(todayMarkets, 'America/Lima');
 
+    const handleSelectChange = (selectedOption) => {
+        if (selectedOption) {
+            setIncidentes([selectedOption.data]);
+        } else {
+            obtenerIncidentes(); // Resetear los incidentes si se borra la selección
+        }
+    };
+
     useEffect(() => {
         const storedUsername = localStorage.getItem('username');
         if (storedUsername) {
             const username = storedUsername.split('@')[0];
             setUsernamePart(username);
             setUsername1(storedUsername);
-            
         } else {
             console.log("No se encontró el nombre de usuario en el localStorage.");
         }
@@ -82,10 +139,9 @@ export default function contentSubirImagenMoto() {
                 </div>
                 <div style={{ display: "block", justifyContent: "center", textAlign: "center", paddingLeft: "20px", paddingRight: "20px" }}>
                     <Select
-                        /*value={selectedDocument}
-                        onChange={handleChange}
-                        options={documentos}*/
-                        placeholder="Busqueda DNI OC Nombre"
+                        options={options}
+                        onChange={handleSelectChange}
+                        placeholder="Busqueda ID, Cliente, Teléfono"
                         isClearable={true}
                     />
                 </div>
@@ -98,15 +154,29 @@ export default function contentSubirImagenMoto() {
                                 <h5>({incidente.id}){" "}{incidente.client}</h5>
                                 <h5><i style={{ fontSize: "15px" }} className='fas fa-phone'></i>{incidente.phone}</h5>
                             </div>
-                            <div style={{ width: "30%", paddingLeft:"5px"}}>
-                                <Button
-                                    variant="contained"
-                                    style={{
-                                        height: "55px",
-                                    }}
-                                >
-                                    Subir
-                                </Button>
+                            <div style={{ width: "30%", paddingLeft: "5px" }}>
+                                {incidente.photo !== null ? (
+                                    <Button
+                                        variant="contained"
+                                        style={{
+                                            height: "55px",
+                                            backgroundColor: "yellow"
+                                        }}
+                                    >
+                                        <i className='fas fa-image'></i>
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        variant="contained"
+                                        style={{
+                                            height: "55px",
+                                            fontSize: "20px"
+                                        }}
+                                        onClick={() => handleOpenUploadModal(incidente)}
+                                    >
+                                        <i className='fas fa-camera'></i>
+                                    </Button>
+                                )}
                             </div>
                         </div>
                         <div style={{ paddingLeft: "10px", }}>
@@ -122,8 +192,47 @@ export default function contentSubirImagenMoto() {
                         </div>
                     </div>
                 </div>
-                )
-            )}
+            ))}
+            <Modal open={modalUpOpen} onClose={handleCloseUpModal}>
+                <div className="modalDetalle" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', overflow: 'auto', maxHeight: '80vh' }}>
+                    <h3 className="card-title">
+                        <b>Subir Imagen de evidencia</b>
+                    </h3>
+                    <br />
+                    <form onSubmit={handleSubmit} style={{ marginTop: '20px' }}>
+                        <TextField
+                            label="ID"
+                            variant="outlined"
+                            value={idImg}
+                            onChange={(e) => setIdImg(e.target.value)}
+                            style={{ marginBottom: '10px', display: 'none'}}
+                        />
+                        <br />
+                        <TextField
+                            label="Cliente"
+                            variant="outlined"
+                            value={selectedIncident ? selectedIncident.client : ''}
+                            disabled
+                            style={{ marginBottom: '10px' }}
+                        />
+                        <br />
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                            style={{ marginBottom: '10px' }}
+                        />
+                        <br />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                        >
+                            Enviar
+                        </Button>
+                    </form>
+                </div>
+            </Modal>
         </div>
     );
 }
